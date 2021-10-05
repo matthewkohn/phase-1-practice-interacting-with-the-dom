537d9fa58e50f6250c19b910e3c91ceb1ea7889e
Matthew <kohney319m@gmail.com>
2021-10-05 13:40:33 -0600

    Add timer and incrementer event listeners

---
+++ js/challenge.js
@@ -0,0 +1,10 @@
+// Definitions
+const counter = document.querySelector('#counter');
+const minus
+const plus
+const heart
+const pause
+const likes
+const comments
+const commentInput
+const submit
